b470aa3725a4f0d7295ef699e2915e8f6cb79f54
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-07-29 03:19:39 +0530

    Update wt-mail.sh

--- wt-mail.sh
+++ wt-mail.sh
@@ -5,4 +5,10 @@ pkg install figlet
 pkg install php
 gem install lolcat
 cd /sdcard/
-
+mkdir php
+cd
+cd tigerz-email
+ls
+cp -r mail.php /sdcard/php/
+cd /sdcard/php/
+php -S localhost:4444 -t /sdcard/php
