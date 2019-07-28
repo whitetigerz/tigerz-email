d6465aca5670487cb7edba8fe68a68a37aa289a1
whitetigerz <50932142+whitetigerz@users.noreply.github.com>
2019-07-29 04:08:45 +0530

    Update wt-mail.sh

--- wt-mail.sh
+++ wt-mail.sh
@@ -9,9 +9,15 @@ mkdir php
 cd
 cd tigerz-email
 ls
+cp -r tigerz##.html /sdcard/
 cp -r index.php /sdcard/php/
 cp -r wt.jpg /sdcard/php/
-cd /sdcard/php/
+cd
+cd /sdcard/
+mv tigerz##.html Tigerz-email.apk
+cd
+ls
+cd tigerz-email
 figlet Prince Yasin |lolcat|lolcat
 figlet Email-Spoofer |lolcat|lolcat
 php -S localhost:4444 -t /sdcard/php
